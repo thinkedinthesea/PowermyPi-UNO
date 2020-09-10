fea3df93a3635f199c917941ab02fd1cc5b22b65
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2020-09-10 14:02:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,15 +10,13 @@
 #
 # FEATURE:
 # - Install/Remove PowermyPi script
-# - Install/Remove PowermyPi Network Module script
 #
 # COMMAND LINE:
 # 1 Install PowermyPi script
 # 2 Remove PowermyPi script
-# 3 Install PowermyPi Network Module
-# 4 Remove PowermyPi Network Module
 #
-REV="rev1-260419"
+#REV="rev1-260419"
+REV="rev2-100920"
 printf "\033c"
 #check sudo
 if [ "$(whoami)" != "root" ] 
@@ -43,32 +41,15 @@ case "$1" in
 	2)
 		#check if the cronjob exist ################################
 		#remove the cronjob#########################################
-		crontab -l | grep -v "@reboot sudo /usr/bin/python /home/pi/PowermyPi/powermypi_uno.py &" | crontab -
+		crontab -l | grep -v "@reboot sudo /usr/bin/python /home/pi/PowermyPi-UNO/powermypi_uno.py &" | crontab -
 		echo "Well Done. Reboot and remove PowermyPi board."
 		exit 1
-		;;
-	3)
-		#executable ###############################################
-		chmod u+x powermypi_network.py
-		#create the cron ##########################################
-		crontab -l | { cat; echo "@reboot sudo /usr/bin/python /home/pi/PowermyPi-UNO/powermypi_network.py &"; } | crontab -
-		echo "Well Done. Reboot to use the Network Module."
-		exit 1
-		;;
-	4)
-		#check if the cronjob exist ################################ 
-                #remove the cronjob#########################################
-                crontab -l | grep -v "@reboot sudo /usr/bin/python /home/pi/PowermyPi-UNO/powermypi_network.py &" | crontab -
-                echo "Well Done. Reboot to remove the Network Module."
-                exit 1
 		;;
 	*)
 		echo "Invalid Argument."
 		echo "Run this command with one of the following options:"
 		echo "1 Install PowermyPi script"
 		echo "2 Remove PowermyPi script"
-		echo "3 Install PowermyPi Network Module"
-		echo "4 Remove PowermyPi Network Module"
 		exit 1
 		;;
 esac
